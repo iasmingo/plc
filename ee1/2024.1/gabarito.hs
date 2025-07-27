6922762d7a597a4a4e438172e5fba868134c2421
Iasmin <iasmin.ufpe@gmail.com>
2025-07-27 14:18:34 -0300

    Correção: Remoção do uso de bibliotecas

--- ee1/2024.1/gabarito.hs
+++ ee1/2024.1/gabarito.hs
@@ -1,41 +1,50 @@
-import qualified Data.Map as Map
-import qualified Data.Set as Set
-
 -- Questão 1
 
--- (a) 
-type Projetos = Map.Map String (Set.Set String)
-
--- Exemplo de dados conforme o enunciado
-projetos :: Projetos
-projetos = Map.fromList [
-    ("p1", Set.fromList ["a", "b"]),
-    ("p2", Set.fromList ["a", "c", "d"]),
-    ("p3", Set.fromList ["e", "f", "g"])]
+-- (a)
+type Projeto = String
+type Pessoa = String
+type Equipe = [Pessoa]
+type Projetos = [(Projeto, Equipe)]
 
 -- (b)
-criaProjeto :: String -> String -> Projetos -> Projetos
-criaProjeto nomeProjeto pessoa projetos =
-    if Map.member nomeProjeto projetos
-    then projetos  -- Projeto já existe, mantém coleção inalterada
-    else Map.insert nomeProjeto (Set.singleton pessoa) projetos  -- Cria novo projeto
+criaProjeto :: Projeto -> Pessoa -> Projetos -> Projetos
+criaProjeto projeto pessoa projetos =
+    if contemProjeto projeto projetos
+        then projetos
+        else (projeto, [pessoa]) : projetos
+    where
+        contemProjeto _ [] = False
+        contemProjeto p ((pId, _):ps)
+            | p == pId = True
+                | otherwise = contemProjeto p ps
 
 -- (c)
-equipe :: String -> Projetos -> Set.Set String
-equipe nomeProjeto projetos =
-    Map.findWithDefault Set.empty nomeProjeto projetos
+equipe :: Projeto -> Projetos -> Equipe
+equipe _ [] = []
+equipe projeto ((p, eq):ps)
+    | projeto == p = eq
+    | otherwise = equipe projeto ps
 
 -- (d)
-naEquipe :: String -> String -> Projetos -> Bool
-naEquipe nomeProjeto pessoa projetos =
-    Set.member pessoa (equipe nomeProjeto projetos)
+naEquipe :: Projeto -> Pessoa -> Projetos -> Bool
+naEquipe _ _ [] = False
+naEquipe projeto pessoa ((p, equipe):ps)
+    | projeto == p = naEquipeAux pessoa equipe
+    | otherwise = naEquipe projeto pessoa ps
 
--- (e)
-acrescentarPessoa :: String -> String -> Projetos -> Projetos
-acrescentarPessoa pessoa nomeProjeto projetos =
-    if Map.member nomeProjeto projetos
-    then Map.insert nomeProjeto (Set.insert pessoa (equipe nomeProjeto projetos)) projetos
-    else projetos
+-- Função auxiliar
+naEquipeAux :: Pessoa -> Equipe -> Bool
+naEquipeAux _ [] = False
+naEquipeAux pessoa (x:xs)
+    | pessoa == x = True
+    | otherwise = naEquipeAux pessoa xs
+
+-- (d)
+acrescentarPessoa :: Projeto -> Pessoa -> Projetos -> Projetos
+acrescentarPessoa _ _ [] = []
+acrescentarPessoa projeto pessoa ((p, equipe):ps)
+    | projeto == p = (p, pessoa : equipe) : ps
+    | otherwise = (p, equipe) : acrescentarPessoa projeto pessoa ps
 
 -- Questão 2
 
@@ -43,9 +52,8 @@ data Nat = Zero | Succ Nat deriving (Eq, Show)
 
 -- (a)
 int2Nat :: Int -> Nat
-int2Nat n 
-    | n == 0 = Zero
-    | otherwise = Succ (int2Nat (n - 1))
+int2Nat 0 = Zero
+int2Nat n = Succ (int2Nat (n-1))
 
 -- (b)
 nat2Int :: Nat -> Int
@@ -54,7 +62,7 @@ nat2Int (Succ n) = 1 + nat2Int n
 
 -- (c)
 soma :: Nat -> Nat -> Nat
-soma a b = int2Nat (nat2Int a + nat2Int b) 
+soma a b = int2Nat (nat2Int a + nat2Int b)
 
 -- (d)
 somaInt :: Nat -> Nat -> Int
